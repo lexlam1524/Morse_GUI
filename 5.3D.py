4a45015727cba770174e3f67fcc81e5e102a5d5f
lexlam1524 <112596172+lexlam1524@users.noreply.github.com>
2023-06-04 22:22:39 +1000

    Update 5.3D.py

--- 5.3D.py
+++ 5.3D.py
@@ -1,18 +1,16 @@
 import tkinter as tk
 from gpiozero import LED
-import RPi.GPIO 
-RPi.GPIO.setmode(RPi.GPIO.BCM)
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2, GPIO.OUT)
+GPIO.output(2, GPIO.LOW)
 import time
 
 
-dot = 0.1
-dash = 0.3
-space = 0.1
-letterSpace = 0.2
-wordspace = 0.5
+
 led = LED(2)
 
-MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+morse_code = { 'A':'.-', 'B':'-...',
                     'C':'-.-.', 'D':'-..', 'E':'.',
                     'F':'..-.', 'G':'--.', 'H':'....',
                     'I':'..', 'J':'.---', 'K':'-.-',
@@ -20,53 +18,30 @@ MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
                     'O':'---', 'P':'.--.', 'Q':'--.-',
                     'R':'.-.', 'S':'...', 'T':'-',
                     'U':'..-', 'V':'...-', 'W':'.--',
-                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
-                    '1':'.----', '2':'..---', '3':'...--',
-                    '4':'....-', '5':'.....', '6':'-....',
-                    '7':'--...', '8':'---..', '9':'----.',
-                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
-                    '?':'..--..', '/':'-..-.', '-':'-....-',
-                    '(':'-.--.', ')':'-.--.-'}
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..'
+                    }
 					
-def encrypt(word):
-    cipher = ''
-    for letter in word:
-        if letter.upper() != ' ':
- 
-            # Looks up the dictionary and adds the
-            # corresponding morse code
-            # along with a space to separate
-            # morse codes for different characters
-            cipher += MORSE_CODE_DICT[letter.upper()] + ' '
-        else:
-            # 1 space indicates different characters
-            # and 2 indicates different words
-            cipher += ' '
- 
-    return cipher
-	
-
-def blinkName():
-    word = text_box.get()
-    cipher = encrypt(word)
-    for letter in cipher:
-        if letter == '.':
-            led.on()
-            time.sleep(dot)
-            led.off()
-            time.sleep(space)
-        elif letter == '-':
-            led.on()
-            time.sleep(dot)
-            led.off()
-            time.sleep(space)
-        elif letter == ' ':
-            led.on()
-            time.sleep(wordspace)
-            led.off()
-            time.sleep(space)
-        time.sleep(letterSpace)
-        
+def blink_led(duration):
+    GPIO.output(2, GPIO.HIGH)
+    time.sleep(duration)
+    GPIO.output(2, GPIO.LOW)
+    time.sleep(0.2) # Delay between blinks
+    
+def convert_to_morse(word):
+    for char in word.upper():
+        if char == ' ':
+            time.sleep(0.6)  # Delay between words
+        elif char in morse_code:
+            code = morse_code[char]
+            for c in code:
+                if c == '.':
+                    blink_led(0.2)  # Blink LED for dot
+                elif c == '-':
+                    blink_led(0.6)  # Blink LED for dash
+                    
+def button_click():
+    input_word = text_box.get()
+    convert_to_morse(input_word)        
        
 root = tk.Tk()
 root.title('Morse Code Generator')
@@ -74,7 +49,7 @@ root.title('Morse Code Generator')
 text_box = tk.Entry(root, width=12)
 text_box.pack(pady=10)
 
-button = tk.Button(root, text='Generate Morse Code', command=blinkName)
+button = tk.Button(root, text='Generate Morse Code', command=button_click)
 button.pack(pady=10)
 
 root.mainloop()
